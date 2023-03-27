8c506c78fba177cbb4e3b532ed2d5470c6e3ed23
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-27 13:30:38 -0700

    fixed error

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100]'>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Image src="/../public/assets/navLogo.png" alt="/" width='125' height='50' />
-            </div>
             <div>
                 <ul className='hidden md:flex'>
                     <Link href=''>
@@ -27,6 +27,12 @@ const Navbar = () => {
                     <li className='ml-10 text-sm uppercase hover:border-b'>contact </li>
                     </Link>
                 </ul>
+                <div className='md:hidden'> 
+                    <AiOutlineMenu size={25}/>
+                </div>
+            </div>
+            {/* <div className='fixed left-0 top-0 w-full h-screen bg-black/70'> */}
+                
             </div>
         </div>
     )
