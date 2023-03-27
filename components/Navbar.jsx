43168067ebb4dc302e94e1a60be28da16707cdf0
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-27 14:09:57 -0700

    addedl linkedin icon, added responsive side bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-
+import {FaLinkedinIn} from 'react-icons/fa'
 const Navbar = () => {
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100]'>
@@ -39,8 +39,36 @@ const Navbar = () => {
                     <div>
                         <div className='flex w-full items-center justify-between'>
                             <Image src='/../public/assets/navLogo.png' width='87' height='35' alt='/' />
-                            <div>
+                            <div className='rounded-full shadow-lg shadow-gray-400 cursor-pointer'>
                                 <AiOutlineClose/>
+                            </div>
+                        </div>
+                        <div className='border-b border-gray-300 my-4'>
+                            <p className='w-[85%] md:w-[90%] py-4'>Let's build something together </p>
+                        </div>
+                    </div>
+                    <div className='py-4 flex flex-col'>
+                        <ul className='uppercase'>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Home </li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>About </li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Skills</li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Projects </li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Contact</li>
+                            </Link>
+                        </ul>
+                        <div className='pt-40'>
+                            <p className='uppercase tracking-widest text-[#5651e5]'> Lets connect</p>
+                            <div>
+                                <FaLinkedinIn />
                             </div>
                         </div>
                     </div>
