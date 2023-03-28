a26b8fe7f2f390f788494f414f2135b107bef4f9
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-28 08:21:10 -0700

    contact form right side ui set up

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -21,29 +21,59 @@ const Contact = () => {
                                 <p>Full-Stack Developer</p>
                                 <p className='py-4'>I am available for freelance or full-stime positions. Contact me and lets talk.</p>
                             </div>
-                        <div>
-                            <p className='uppercase py-4'>Connect With Me</p>
-                            <div className='flex items-center justify-between py-4'>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                    <FaLinkedinIn />
-                                </div>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                    <FaGithub />
-                                </div>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                    <AiOutlineMail />
-                                </div>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                                    <BsFillPersonLinesFill />
-                                </div>
+                            <div>
+                                <p className='uppercase py-4'>Connect With Me</p>
+                                <div className='flex items-center justify-between py-4'>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <FaLinkedinIn />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <FaGithub />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <AiOutlineMail />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <BsFillPersonLinesFill />
+                                    </div>
 
+                                </div>
                             </div>
-                        </div>
                         </div>
 
                     </div>
 
 
+                    {/* Right side */}
+                    <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+                        <div className='p-4'>
+                            <form>
+                                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Name</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Phone Number</label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                    </div>
+                                </div>
+                                <div className='flex flex-col py-2'>
+                                <label className='uppercase text-sm py-2'>Email</label>
+                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" />
+                                </div>
+                                <div className='flex flex-col py-2'>
+                                <label className='uppercase text-sm py-2'>Subject</label>
+                                <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                </div>
+                                <div className='flex flex-col py-2'>
+                                <label className='uppercase text-sm py-2'>Message</label>
+                                <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10'/>
+                                </div>
+                                <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
