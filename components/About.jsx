226948b45ade5e1783d27678c514f9a7224f46c1
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-28 11:42:17 -0700

    headshot inserted

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import Headshot from '../public/assets/projects/headshot.jpg'
 
 const About = () => {
     return (
@@ -14,7 +16,7 @@ const About = () => {
 
                 </div>
                 <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                    <img className='rounded-xl' src="png" />
+                    <Image className='rounded-xl' src={Headshot} alt={Headshot} />
                 </div>
             </div>
         </div>
