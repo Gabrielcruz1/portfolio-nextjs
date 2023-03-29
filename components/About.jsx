643b48f0097bd27a1293028d6b0c0e86ca6413a8
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-28 17:38:20 -0700

    removed bio

--- components/About.jsx
+++ components/About.jsx
@@ -11,20 +11,8 @@ const About = () => {
                     <p className='uppercase text-xl tracking-widest text-[#5651e5]'> About</p>
                     <h2 className='py-4'> Who am I</h2>
                     <p className='py-2 text-gray-600'> I'm not your normal developer</p>
-                    <p className='py-2 text-greay-600'>   I specialize in building mobile responsive front-end UI applications
-                        that connect with API’s and other backend technologies. I’m
-                        passionate about learning new technologies and understand there is
-                        more than one way to accomplish a task. Though I am most proficient
-                        in building front-end applications using HTML, CSS, Javascript, and
-                        React, I am a quick learner and can pick up new tech stacks as
-                        needed. I believe that being a great developer is not using one
-                        specific language, but choosing the best tool for the job. </p>
-                    <p className='py-2 text-greay-600'> I started web developement in 2013 managing multiple e-commerce
-                        websites on CMS platforms such as WordPress, BigCommerce, and
-                        Shopify. I have experience working directly with clients and taking
-                        mock wireframes all the way to deployed applications. In my spare
-                        time I run Code Commerce, a Youtube channel where I teach web
-                        developement and various front-end technologies. </p>
+                    <p className='py-2 text-greay-600'>  </p>
+                    <p className='py-2 text-greay-600'>  </p>
                     <Link href='/#projects'>
                         <p className='py-2 text-greay-600 underline cursor-pointer'> Check out my latest projects!</p>
                     </Link>
